696745faa4927ad8f51a63795396ac3f712ce30a
lemegudata@gmail.com <146845168+Lamibane@users.noreply.github.com>
2024-03-10 09:23:45 +0300

    yes

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 require("dotenv").config();
 const express = require("express");
-<<<<<<< HEAD
 const cors = require("cors")
 const app = express();
-=======
 const cors = require("cors");
->>>>>>> 629da9f91978e2cffeaf5ab660a4fa7dace1e6d3
 const port = 4000 || process.env.PORT;
 const userRouter = require("./api/users/user.router");
 const questionRouter = require("./api/questions/question.router");
