8531497d2bc0ad0786846ed00e95a69dd5abc20e
lemegudata@gmail.com <146845168+Lamibane@users.noreply.github.com>
2024-03-15 23:20:48 +0300

    deployment on free

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 //configuration of connection to mysql server
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors")
 const app = express();
 app.use(cors());
+//configuration
 const port = 4000 || process.env.PORT;
 const userRouter = require("./api/users/user.router");
 const questionRouter = require("./api/questions/question.router");
